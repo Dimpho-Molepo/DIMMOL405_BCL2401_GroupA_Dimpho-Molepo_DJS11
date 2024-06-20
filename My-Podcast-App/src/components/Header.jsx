90268597f8d615cd4f7396f41e44d02108c4f134
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-06-20 17:54:33 +0200

    Removed the seach from the Header and place it on the Home component

--- My-Podcast-App/src/components/Header.jsx
+++ My-Podcast-App/src/components/Header.jsx
@@ -3,9 +3,8 @@ import { Link, NavLink } from "react-router-dom"
 import "./CSS/Header.css"
 import logo from "../assets/Podcast-Logo2.svg"
 import { MdFavorite } from "react-icons/md";
-import { IconContext } from "react-icons";
 import { FaArchive } from "react-icons/fa";
-import Search from "../components/Search"
+
 
 
 export default function Header() {
@@ -18,35 +17,32 @@ export default function Header() {
     return (
         <header>
             <Link  to="/">
-                <img className="site-logo" src={logo} alt="" />
+                <img className="site-logo" src={logo} alt={logo} />
             </Link>
 
             <div>
                 
-                <Search/>
-
                 <nav>
 
-                <NavLink
-                    to="/Favourites"
-                    style={({ isActive }) => isActive ? activeStyles : null}
-                >
-                    <MdFavorite className="favourites" />
-                   
-                </NavLink>
-
-                <NavLink
-                    to="/History"
-                    style={({ isActive }) => isActive ? activeStyles : null}
-                >
-                    <FaArchive  className="archive"/>
+                    <NavLink
+                        to="/Favourites"
+                        style={({ isActive }) => isActive ? activeStyles : null}
+                    >
+                        <MdFavorite className="favourites" />
                     
-                </NavLink>
+                    </NavLink>
+
+                    <NavLink
+                        to="/History"
+                        style={({ isActive }) => isActive ? activeStyles : null}
+                    >
+                        <FaArchive  className="archive"/>
+                        
+                    </NavLink>
 
                 </nav>
             </div>
-            
-            
+             
         </header>
     )
 }
