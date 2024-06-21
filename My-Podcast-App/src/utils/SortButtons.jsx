b044213af1a22aeb4d92be5c88ebf08e00b1c499
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-06-21 09:59:11 +0200

    Made a component for the sort buttons

---
+++ My-Podcast-App/src/utils/SortButtons.jsx
@@ -0,0 +1,43 @@
+// SortButtons.jsx
+import React from 'react';
+
+const SortButtons = ({ sortShows, onSortChange, onClear }) => {
+  return (
+    <div className="sort-shows-div">
+      <button 
+        className={`sort-button ${sortShows === "A-Z" ? "selected-sort-button" : ""}`} 
+        onClick={() => onSortChange("A-Z")}
+      >
+        A-Z
+      </button>
+      <button 
+        className={`sort-button ${sortShows === "Z-A" ? "selected-sort-button" : ""}`} 
+        onClick={() => onSortChange("Z-A")}
+      >
+        Z-A
+      </button>
+      <button 
+        className={`sort-button ${sortShows === "Newest" ? "selected-sort-button" : ""}`} 
+        onClick={() => onSortChange("Newest")}
+      >
+        Newest
+      </button>
+      <button 
+        className={`sort-button ${sortShows === "Oldest" ? "selected-sort-button" : ""}`} 
+        onClick={() => onSortChange("Oldest")}
+      >
+        Oldest
+      </button>
+      {sortShows && (
+        <button
+          onClick={onClear}
+          className="clear-button"
+        >
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default SortButtons;
