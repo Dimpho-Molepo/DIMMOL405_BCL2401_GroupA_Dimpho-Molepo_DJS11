46d2afd1594e91076db627822bd4f07c8d17321a
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-06-21 09:59:57 +0200

    moved the genre into the utils folder

---
+++ My-Podcast-App/src/utils/Genre.js
@@ -0,0 +1,15 @@
+export const genresObject = {
+    1: "Personal Growth 🪴",
+    2: "Investigative Journalism 🔎",
+    3: "History 📚",
+    4: "Comedy 🤣",
+    5: "Entertainment 🧑‍🎤",
+    6: "Business 🧑‍💼",
+    7: "Fiction 📗",
+    8: "News 📰",
+    9: "Kids and Family 👶"
+};
+
+export function genreInfo(genreIds) {
+    return genreIds.map((id) => genresObject[id]).join(", ");
+}
